88ad8f9e4309085bf70469d30d5c9033803a543a
EchoFUN <xukai.ken@gmail.com>
2019-08-28 20:40:03 +0800

    ADD

--- src/main/java/Service.java
+++ src/main/java/Service.java
@@ -1,9 +1,11 @@
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.Map;
 
 import database.DBUtils;
 import database.PostModel;
 import nioserver.IMessageProcessor;
+import nioserver.Message;
 import nioserver.Server;
 import nioserver.http.HttpMessageReaderFactory;
 
@@ -11,32 +13,26 @@ import nioserver.http.HttpMessageReaderFactory;
  *
  */
 public class Service {
-    static final String httpResponse = "HTTP/1.1 200 OK\r\nContent-Length: 38\r\nContent-Type: text/html\r\n\r\n<html><body>Hello World!</body></html>";
 
     public static void main(String[] args) throws IOException {
-        byte[] httpResponseBytes = httpResponse.getBytes("UTF-8");
-
         PostModel postModel = new PostModel();
         IMessageProcessor messageProcessor = (request, writeProxy) -> {
             System.out.println("Message Received from socket: " + request.socketId);
-
             Map<String, String> post = postModel.fetchPostByID(7);
 
-
-            /*
             Message response = writeProxy.getMessage();
             response.socketId = request.socketId;
-            response.writeToMessage(httpResponseBytes);
-
+            String feedback = "HTTP/1.1 200 OK\r\nContent-Length: 38\r\nContent-Type: text/html\r\n\r\n<html><body>" + post.get("date") + "</body></html>";
+            try {
+                response.writeToMessage(feedback.getBytes("UTF-8"));
+            } catch (UnsupportedEncodingException e) {
+                ;
+            }
             writeProxy.enqueue(response);
-            */
         };
 
         DBUtils.inst().init();
-
         Server server = new Server(9999, new HttpMessageReaderFactory(), messageProcessor);
-
-
         server.start();
     }
 }
